a869face36aca4fdf75ed25577a9e19e658e6084
Nathan Stryker <nathan.stryker@optum.com>
2022-01-27 14:38:14 -0600

    initial submission

---
+++ Demo_CICD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo_CICD
+//
+//  Created by Stryker, Nathan P on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo_CICD/Demo_CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_CICDApp.swift
+//  Demo_CICD
+//
+//  Created by Stryker, Nathan P on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Demo_CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
