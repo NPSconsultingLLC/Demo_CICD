f0234d9fb1e7c0de8be84f6e9726d0518f565593
Nathan Stryker <nathan.stryker@optum.com>
2022-01-28 10:05:17 -0600

    adding email validator and simple login screen

--- Demo_CICD/ContentView.swift
+++ Demo_CICD/ContentView.swift
@@ -7,15 +7,114 @@
 
 import SwiftUI
 
-struct ContentView: View {
+let storedUsername = "OptumUser@optum.com"
+let storedPassword = "Test2Day"
+
+struct ContentView : View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                WelcomeText()
+                UserImage()
+                UsernameTextField(username: $username)
+                PasswordTextField(password: $password)
+                
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    if self.username == storedUsername && self.password == storedPassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    } else {
+                        self.authenticationDidFail = true
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+            } .padding()
+            if authenticationDidSucceed {
+                Text("Login succeeded!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.green)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                    .animation(.default, value: 0)
+            }
+        }
     }
 }
 
+struct WelcomeText : View {
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+struct UserImage : View {
+    var body: some View {
+        VStack {
+            Image(systemName: "person.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .padding(.bottom, 75)
+        }.foregroundColor(.blue)
+    }
+}
+
+struct LoginButtonContent : View {
+    var body: some View {
+        VStack {
+            Text("LOGIN")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+        }
+    }
+}
+
+struct UsernameTextField: View {
+    @Binding var username: String
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.90))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordTextField: View {
+    @Binding var password: String
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.90))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
